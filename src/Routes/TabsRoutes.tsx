e1a34e3445fc6000dbbd210a048af726fc5a6c29
LTilio <Leandro.tilio@gmail.com>
2024-06-21 19:58:53 -0300

    Fix: Ajuste no ultimo commit

--- src/Routes/TabsRoutes.tsx
+++ src/Routes/TabsRoutes.tsx
@@ -1,8 +1,10 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { AreaAluno } from "../Pages/AreaAluno";
+import { Home } from "../Pages/Home";
 
 export type RootTabParamsList = {
+  TabHome: { name: string };
   TabAreaAluno: { name: string };
 };
 
@@ -11,6 +13,7 @@ const Tab = createBottomTabNavigator<RootTabParamsList>();
 export function TabsRoutes() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="TabHome" component={Home} />
       <Tab.Screen name="TabAreaAluno" component={AreaAluno} />
     </Tab.Navigator>
   );
