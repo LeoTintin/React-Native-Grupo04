037948734986fddffc1d60675fe88ba4b3b48825
Luiz1210 <gusazevg@gmail.com>
2024-06-21 19:48:22 -0300

    Feat: ALteraçoes Home

--- src/Routes/TabsRoutes.tsx
+++ src/Routes/TabsRoutes.tsx
@@ -1,10 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { AreaAluno } from "../Pages/AreaAluno";
-import { Home } from "../Pages/Home";
 
 export type RootTabParamsList = {
-  TabHome: { name: string};
   TabAreaAluno: { name: string };
 };
 
@@ -13,7 +11,6 @@ const Tab = createBottomTabNavigator<RootTabParamsList>();
 export function TabsRoutes() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="TabHome" component = {Home}></Tab.Screen>
       <Tab.Screen name="TabAreaAluno" component={AreaAluno} />
     </Tab.Navigator>
   );
